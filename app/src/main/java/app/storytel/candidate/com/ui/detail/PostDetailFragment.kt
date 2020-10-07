44ad23e7d9ef544ae79da71b512ed47b1b2c41cb
oGonzaga <yela7laM>
2020-10-07 02:23:51 -0300

    Implemented loading bar.

--- app/src/main/java/app/storytel/candidate/com/ui/detail/PostDetailFragment.kt
+++ app/src/main/java/app/storytel/candidate/com/ui/detail/PostDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
@@ -11,6 +12,9 @@ import app.storytel.candidate.com.R
 import app.storytel.candidate.com.data.model.Comment
 import app.storytel.candidate.com.data_remote.ResourceList
 import app.storytel.candidate.com.data_remote.model.Status
+import app.storytel.candidate.com.ui.extensions.gone
+import app.storytel.candidate.com.ui.extensions.invisible
+import app.storytel.candidate.com.ui.extensions.visible
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_post_detail.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -22,14 +26,18 @@ class PostDetailFragment : Fragment() {
     private val requestObserver = Observer<ResourceList<Comment>> {
         when (it.status) {
             Status.SUCCESS -> {
+                pbLoading.gone()
+                rvComment.visible()
                 val adapter =  CommentAdapter(it.data!!)
                 rvComment.adapter = adapter
             }
             Status.ERROR -> {
-                println()
+                pbLoading.gone()
+                Toast.makeText(context, getString(R.string.error_text), Toast.LENGTH_LONG).show()
             }
             Status.LOADING -> {
-
+                rvComment.invisible()
+                pbLoading.visible()
             }
         }
     }
