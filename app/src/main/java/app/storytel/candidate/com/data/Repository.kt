7ad1735759b0b2a642acbd9fc59d1dcab9c84e48
oGonzaga <yela7laM>
2020-10-07 00:51:49 -0300

    Implemented get comments api call.

--- app/src/main/java/app/storytel/candidate/com/data/Repository.kt
+++ app/src/main/java/app/storytel/candidate/com/data/Repository.kt
@@ -1,10 +1,12 @@
 package app.storytel.candidate.com.data
 
+import app.storytel.candidate.com.data.model.Comment
 import app.storytel.candidate.com.data.model.Photo
 import app.storytel.candidate.com.data.model.Post
-import app.storytel.candidate.com.data_remote.Resource
+import app.storytel.candidate.com.data_remote.ResourceList
 
 interface Repository {
-    suspend fun getPosts(): Resource<List<Post>>
-    suspend fun getPhotos(): Resource<List<Photo>>
+    suspend fun getPosts(): ResourceList<Post>
+    suspend fun getPhotos(): ResourceList<Photo>
+    suspend fun getComments(postId: String): ResourceList<Comment>
 }
