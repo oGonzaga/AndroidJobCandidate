254ed6b6bbfec0281aa914f1e44fa8b9c5ff1f85
oGonzaga <yela7laM>
2020-10-07 00:00:13 -0300

    Fix navigation and sent post model to PostDetailFragment.kt

--- app/src/main/java/app/storytel/candidate/com/ui/detail/PostDetailFragment.kt
+++ app/src/main/java/app/storytel/candidate/com/ui/detail/PostDetailFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import app.storytel.candidate.com.R
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_post_detail.*
 
 class PostDetailFragment : Fragment() {
+
+    private val args: PostDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,4 +20,17 @@ class PostDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_post_detail, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupView()
+    }
+
+    private fun setupView() {
+        args.post.let {
+            details.text = it.title
+                Glide.with(this)
+                    .load(it.imageUrl)
+                    .placeholder(R.drawable.ic_baseline_android)
+                    .into(backdrop)
+        }
+    }
 }
